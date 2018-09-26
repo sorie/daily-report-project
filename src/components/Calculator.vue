57e753920340e90d294ab8602cc95ece09687665
sorie <enne3939@gmail.com>
2018-09-26 11:38:41 +0900

    세로선택 후 class 추가

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="calculator">
     <div class="display">0.05324</div>
-    <div>C</div>
-    <div>+/-</div>
-    <div>%</div>
-    <div>/</div>
-    <div>7</div>
-    <div>8</div>
-    <div>9</div>
-    <div>x</div>
-    <div>4</div>
-    <div>5</div>
-    <div>6</div>
-    <div>-</div>
-    <div>1</div>
-    <div>2</div>
-    <div>3</div>
-    <div>+</div>
+    <div class="">C</div>
+    <div class="">+/-</div>
+    <div class="">%</div>
+    <div class="">/</div>
+    <div class="">7</div>
+    <div class="">8</div>
+    <div class="">9</div>
+    <div class="">x</div>
+    <div class="">4</div>
+    <div class="">5</div>
+    <div class="">6</div>
+    <div class="">-</div>
+    <div class="">1</div>
+    <div class="">2</div>
+    <div class="">3</div>
+    <div class="">+</div>
     <div class="zero">0</div>
     <div>.</div>
     <div>=</div>
